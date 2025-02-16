635b7d24da32c9cc8d30de44542b7f7332fcf42d
donghar <donghar@naver.com>
2025-02-17 05:03:15 +0900

    ✨apply Layer

--- src/components/feature/Layout.tsx
+++ src/components/feature/Layout.tsx
@@ -1,10 +1,15 @@
+import MainNavbar from "./MainNavbar";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
     <>
-      <h1>This is Layout!</h1>
-      <Outlet />
+      <header className="fixed inset-x-0 top-0 z-50 bg-gray-800 shadow-md">
+        <MainNavbar />
+      </header>
+      <main className="top-30 fixed inset-x-20 shadow-md">
+        <Outlet />
+      </main>
     </>
   );
 };
